32babdd0bdfd258e275a44dfa749ae330fc26d6e
Daniel Freedman <dfreedm@google.com>
2012-08-17 16:57:51 -0700

    Start test rewrite
    * Add function.bind requirement
    * cover good portion of polyfill.path

--- tests/requirements.js
+++ tests/requirements.js
@@ -1,5 +1,10 @@
 module("Requirements");
 
 test('WebKitShadowRoot is required', 1, function() {
-    ok(!!window.WebKitShadowRoot);
+    equal(typeof window.WebKitShadowRoot, "function");
 });
+
+test("Function.bind exists", 1, function() {
+	ok((function(){}).bind);
+});
+
