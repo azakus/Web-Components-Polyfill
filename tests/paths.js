85969807a87c49a4fe18cd17b1ba998c07858e09
Daniel Freedman <dfreedm@google.com>
2012-08-20 14:40:43 -0700

    add test for makeRelPath

--- tests/paths.js
+++ tests/paths.js
@@ -7,6 +7,23 @@ test("Absolute paths are indentified", 3, function() {
 	});
 });
 
+test("URLs are compressed correctly", function() {
+	var urls = [
+		{ in: "foo/bar/../baz", out: "foo/baz" },
+		{ in: "foo/../bar/baz", out: "bar/baz" },
+		{ in: "foo/bar/baz/..", out: "foo/bar" }
+	];
+	expect(urls.length);
+	urls.forEach(function(u){
+		equal(polyfill.path.compressUrl(u.in), u.out);
+	});
+});
+
+test("makeRelPath makes correct relative paths", 1, function() {
+	var source = "foo/bar/baz/quxx/", target = "foo/bar/baz/zeph";
+	equal(polyfill.path.makeRelPath(source, target), "../zeph");
+});
+
 test("Document URL can be obtained from a node", 1, function() {
 	equal(polyfill.path.documentUrlFromNode(document.body), location.toString());
 });
@@ -28,18 +45,6 @@ test("Source and Href urls are resolved correctly", 2, function() {
 	document.body.removeChild(i);
 	document.body.removeChild(ds);
 	document.body.removeChild(dh);
-});
-
-test("URLs are compressed correctly", function() {
-	var urls = [
-		{ in: "foo/bar/../baz", out: "foo/baz" },
-		{ in: "foo/../bar/baz", out: "bar/baz" },
-		{ in: "foo/bar/baz/..", out: "foo/bar" }
-	];
-	expect(urls.length);
-	urls.forEach(function(u){
-		equal(polyfill.path.compressUrl(u.in), u.out);
-	});
 });
 
 test("CSS urls are relativized correctly", 1, function() {
