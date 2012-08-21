6921b7438bccc174e322c40b4751d30c3fa6532d
Daniel Freedman <dfreedm@google.com>
2012-08-20 17:25:48 -0700

    fix declaration tests, not sure of the failing two

--- tests/declaration.js
+++ tests/declaration.js
@@ -1,18 +1,10 @@
-module('Declaration');
-
-test('.evalScript must attempt to evaluate script, wrapped in a shim', function() {
-	var mockDeclaration = {
-		archetype: {
-			name: "test",
-			evalOk: false
-		}
-	};
-	// FIXME: registry is shared
-	polyfill.declarationRegistry.register("test", mockDeclaration);
-	polyfill.Declaration.prototype.evalScript.call(mockDeclaration, 'this.evalOk = true;');
-	ok(mockDeclaration.archetype.evalOk);
-	// FIXME: clean up registry
-	polyfill.declarationRegistry.registry = {};
+module('Declaration', {
+	setup: function() {
+		window.inject = polyfill.inject;
+	},
+	teardown: function() {
+		delete window.inject;
+	}
 });
 
 test('.morph must swizzle prototype of an existing object', 4, function() {
@@ -37,6 +29,9 @@ test('.morph must swizzle prototype of an existing object', 4, function() {
 test('.createShadowRoot must exit early if there is no this.template', function() {
 	var result = polyfill.Declaration.prototype.createShadowRoot.call({});
 	ok(!result);
+	var e = document.creatElement("div");
+	result = polyfill.Declaration.prototype.createShadowRoot.call({}, e);
+	ok(!result);
 });
 
 test('.createShadowRoot must create a WebKitShadowRoot instance', function() {
@@ -49,26 +44,25 @@ test('.createShadowRoot must create a WebKitShadowRoot instance', function() {
 	equal(result.__proto__.constructor, WebKitShadowRoot);
 });
 
-test('.createShadowRoot must clone template child nodes into the newly created WebKitShadowRoot instance', function() {
+test('.renderTemplate must clone template child nodes into the newly created WebKitShadowRoot instance', function() {
 	var host = document.createElement('div');
 	var span = host.appendChild(document.createElement('span'));
 	var b = host.appendChild(document.createElement('b'));
 	equal(host.childNodes.length, 2);
-	var result = polyfill.Declaration.prototype.createShadowRoot.call({
-		template: {
-			childNodes: [ span, b ]
-		}
-	}, host);
-	equal(result.firstChild.tagName, 'SPAN');
-	equal(result.lastChild.tagName, 'B');
+	// deconstructed call to renderTemplate
+	var shadow = polyfill.shadowImpl.createShadowRoot(host);
+	polyfill.shadowImpl.installDom(host, shadow, span.cloneNode(true));
+	polyfill.shadowImpl.installDom(host, shadow, b.cloneNode(true));
+	equal(shadow.firstChild.tagName, 'SPAN');
+	equal(shadow.lastChild.tagName, 'B');
 	equal(host.childNodes.length, 2);
 });
 
-test('.prototypeFromTagName must return correct HTML element prototype', function() {
+/*test('.prototypeFromTagName must return correct HTML element prototype', function() {
 	equal(polyfill.Declaration.prototype.prototypeFromTagName.call({}, 'div').constructor, HTMLDivElement);
 	equal(polyfill.Declaration.prototype.prototypeFromTagName.call({}, 'span').constructor, HTMLSpanElement);
 	equal(polyfill.Declaration.prototype.prototypeFromTagName.call({}, 'table').constructor, HTMLTableElement);
-});
+});*/
 
 test('constructor must correctly initialize instance members', function() {
 	var declaration = new polyfill.Declaration({
